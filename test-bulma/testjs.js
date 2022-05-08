0918b5cf3666d7a4a3cf679322f9e53f752676f6
flyingwonton <66998126+flyingwonton@users.noreply.github.com>
2022-05-08 22:18:48 +0200

    DOMContentLoaded event

--- test-bulma/testjs.js
+++ test-bulma/testjs.js
@@ -1,19 +1,22 @@
-const noButton = document.getElementById('no');
+document.addEventListener("DOMContentLoaded", function(event) { //al caricamento del DOM (prima di css, immagini etc) viene caricata la funzione contenente i vari eventi da gestire ---> in questo modo, il js può essere messo a caricare sostanzialmente a qualsiasi altezza del HTML
 
-no.addEventListener('mouseover', function handleMouseOver() {
-  noButton.textContent = 'Yes';
+    const noButton = document.getElementById('no');
+
+
+    no.addEventListener('mouseover', function handleMouseOver() {
+      noButton.textContent = 'Yes';
+    });
+    
+    no.addEventListener('mouseout', function handleMouseOut() {
+        noButton.textContent = 'No';
+    });
+    
+    const delButton = document.getElementById('deleteButton');
+    
+    delButton.addEventListener('click', function click(){
+        document.getElementById("messageLarge").style.display = "none";
+        document.getElementById("hiddenPar").innerHTML = "NOOOOOOOOOOOOOOOOOOOOOOO :("
+    });
+
+
 });
-
-no.addEventListener('mouseout', function handleMouseOut() {
-    noButton.textContent = 'No';
-});
-
-const delButton = document.getElementById('deleteButton');
-
-delButton.addEventListener('click', function click(){
-    document.getElementById("messageLarge").style.display = "none";
-    document.getElementById("hiddenPar").innerHTML = "NOOOOOOOOOOOOOOOOOOOOOOO :("
-});
-
-
-
